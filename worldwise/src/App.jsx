0dd043e8e3ed737791b53e4a511f7f7919e61ead
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-11-23 10:53:55 -0600

    added Form to query map, and changed declarative navigation to app

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Product from "./pages/Product";
@@ -9,6 +9,7 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import City from "./components/City";
+import Form from "./components/Form";
 import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
@@ -34,17 +35,14 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter basename="/worldwide">
+    <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -54,7 +52,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
