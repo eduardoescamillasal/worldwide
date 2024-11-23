2e53ab1b9b78ff9c3f7d63a1eaea333506caf06f
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-11-23 09:35:09 -0600

    deploy

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/worldwide">
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
